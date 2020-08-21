eb8971e7bd06d32e597314a012ea3e9f3dd578a2
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-08-21 18:47:08 +0530

    functionality added: Delete user data

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,41 @@
-import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
-const router = express.Router();
-const users = [];
-router.get("/", (req, res) => {
-  res.send(users);
-});
+const router = express.Router()
+let users = []
+router.get('/', (req, res) => {
+  res.send(users)
+})
 
-router.post("/", (req, res) => {
-  const user = req.body;
+router.post('/', (req, res) => {
+  const user = req.body
 
   // const userWithId = { ...user, id: uuidv4() };
 
-  users.push({ ...user, id: uuidv4() });
+  users.push({
+    ...user,
+    id: uuidv4(),
+  })
 
   // users.push(user);
 
-  res.send(`User with the name ${user.firstName} added to the database!`);
-});
+  res.send(`User with the name ${user.firstName} added to the database!`)
+})
 
-router.get("/:id", (req, res) => {
-  res.send("The GET ID Route");
-});
+//  /users/2 => req.params {id:2}
 
-export default router;
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  const findUser = users.find((user) => user.id == id)
+  res.send(findUser)
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  users = users.filter((user) => user.id != id)
+
+  res.send(`User with id ${id} is deleted from the database.`)
+})
+
+export default router
